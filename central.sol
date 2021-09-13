3d7cfe410deae093c4cbaf64d3c091e4cc4deb40
Cheyenne Atapour <cheyenneatapour@gmail.com>
2021-09-13 13:08:06 -0700

    create parametrized central function

--- central.sol
+++ central.sol
@@ -1,30 +1,10 @@
 pragma solidity ^0.8.7;
 
-contract Central {
-    
-   // Declaring state variables
-    int x ;
-    int q ;
-      
-    // Defining a function
-    // to set the value of
-    // the first variable
-    function integer(int val) public 
-    {
-        x = val;
-    }
-      
-    // Defining a function
-    // to set the value of
-    // the second variable
-    function mod(int y) public 
-    {
-        q = y;
-    }
-      
+contract BKLMSigs {
+
     // Defining a function to return
     // the modulus value
-    function Modulo() view public returns (int) 
+    function central(int x, int q) pure public returns (int) 
     {
         int bd = (q-1)/2;
         int tmp = x % (2*bd+1);
@@ -32,8 +12,7 @@ contract Central {
         {
             tmp -= (2*bd+1);
         }
-        
-        
+       
         return tmp;
     }
     
